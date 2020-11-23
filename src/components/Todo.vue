3056027bf0f4d06517d66eed83e3de7b587eb50c
nellyXinwei <nelsonalbajr@yahoo.com>
2020-11-23 15:12:01 +0800

    restart proj with vue2, importing components and passing props

---
+++ src/components/Todo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <h3>{{todo.title}}</h3>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  props: ["todo"]
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Todos</h1>
+
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <Todo v-bind:todo="todo"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Todo from './Todo.vue';
+
+export default {
+  name: "Todos",
+  props: ["todos"],
+  components: {Todo},
+};
+</script>
+
+<style scoped>
+</style>
