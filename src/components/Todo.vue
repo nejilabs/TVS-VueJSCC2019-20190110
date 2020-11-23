9da36d5a18bfc0b82ec2ef288351f6787f76a881
nellyXinwei <nelsonalbajr@yahoo.com>
2020-11-23 15:17:07 +0800

    v-bind:class directive

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,17 +1,36 @@
+```jsx
 <template>
-  <div>
-      <h3>{{todo.title}}</h3>
-
-  </div>
+<div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+  <p>{{ todo.title }}</p>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'Todo',
-  props: ["todo"]
-}
+  name: "Todo",
+  props: ["todo"],
+};
 </script>
 
-<style>
+<style scoped>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px #ccc dotted;
+}
 
+.is-complete {
+  text-decoration: line-through;
+}
+
+.del {
+  background: #ff0000;
+  color: #fff;
+  border: none;
+  padding: 5px 9px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
+}
 </style>
+```
